96164a292b843995cc232bf6834e1e26c488bed4
tangsong1111 <31057584+tangsong1111@users.noreply.github.com>
2026-09-08 17:31:54 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wedding-prep-v2';
+const CACHE_NAME = 'wedding-prep-v3';
 const BASE_URL = new URL('./', self.registration.scope);
 const shellUrl = (asset) => new URL(asset, BASE_URL).toString();
 const INDEX_URL = shellUrl('index.html');
@@ -9,7 +9,7 @@ const APP_SHELL = [
   'icons/icon-180.png',
   'icons/icon-192.png',
   'icons/icon-512.png',
-  'images/couple-countdown.png',
+  'images/couple-countdown.png?v=20260908-4',
   'src/main.js',
   'src/styles/app.css',
   'src/styles/tokens.css'
